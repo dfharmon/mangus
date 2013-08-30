fdeaa255cbbce516f3f199a32d9f2d56655de1ed
amys <amys@vubiquity.com>
2013-08-29 18:32:05 -0600

    hiding things on login and sign up page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,8 +1,19 @@
-$(function() {
-  $("#week_menu a").each(function() {
+$(function () {
+  $("#week_menu a").each(function () {
     var query_string = window.location.search.substring(1);
-    if ($(this).attr('href')  ===  "?" + query_string) {
+    if ($(this).attr('href') === "?" + query_string) {
       $(this).addClass("selected_link");
+    }
+  });
+});
+
+$(function () {
+  $(".hide_on_login").each(function () {
+    if (window.location.pathname == '/users/sign_in' || window.location.pathname == '/users/sign_up') {
+      $(this).hide();
+    }
+    else {
+      $(this).show();
     }
   });
 });
