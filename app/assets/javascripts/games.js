dc68249dfe4e6f2603274807ebaebf1b516172b6
amys <amys@vubiquity.com>
2013-08-30 00:48:18 -0600

    added carrierwave for avatar. styled jquery flash messages

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -25,9 +25,9 @@ $(function () {
 
 
 $(document).ready(function () {
-  //if ($('#error_explanation').text().length > 0) {
+  if ($('#error_explanation').text().length > 0) {
     $('#errors').hide().delay(800).slideDown(800).delay(4000).slideUp(800);
-  //}
+  }
 });
 
 
