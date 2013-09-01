6fc789ee4c4d2380102d4fce4f638255662ac10a
amys <amys@vubiquity.com>
2013-09-01 12:29:54 -0600

    hacked up javascript

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -17,4 +17,31 @@ $(document).ready(function () {
   }
 });
 
+$(document).ajaxComplete(function(event, request) {
+  var msg;
+  if(request.getResponseHeader('X-Flash-Notice')) {
+    msg = request.getResponseHeader('X-Flash-Notice');
+  }
+  else if(request.getResponseHeader('X-Flash-Alert')) {
+    msg = request.getResponseHeader('X-Flash-Alert');
+  }
+  else if(request.getResponseHeader('X-Flash-Error')) {
+    request.getResponseHeader('X-Flash-Error');
+  }
+
+  if (msg) {
+    $('#notice').html(msg);
+  }
+
+  if ($('#notice').text().length > 0) {
+    $('#notices').hide().delay(800).slideDown(800).delay(4000).slideUp(800);
+  }
+});
+
+$(document).ready(function(event, request) {
+  if ($('#notice').text().length > 0) {
+    $('#notices').hide().delay(800).slideDown(800).delay(4000).slideUp(800);
+  }
+});
+
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   #protect_from_forgery
+  after_filter :add_flash_to_header
+
 
   def after_sign_in_path_for(resource)
     latest_week = Game.pluck(:week).sort.uniq.last
     "?week=#{latest_week}"
+  end
+
+  def add_flash_to_header
+    # only run this in case it's an Ajax request.
+    return unless request.xhr?
+
+    # add different flashes to header
+    response.headers['X-Flash-Error'] = flash[:error] unless flash[:error].blank?
+    response.headers['X-Flash-Warning'] = flash[:warning] unless flash[:warning].blank?
+    response.headers['X-Flash-Notice'] = flash[:notice] unless flash[:notice].blank?
+    response.headers['X-Flash-Message'] = flash[:message] unless flash[:message].blank?
+
+    # make sure flash does not appear on the next page
+    flash.discard
   end
 end
