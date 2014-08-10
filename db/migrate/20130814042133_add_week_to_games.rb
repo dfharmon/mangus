0b65166a7f1fe071c1f776921a2b4697a100a2a7
dharmon <dharmon@avail-tvn.com>
2014-08-10 09:47:36 -0600

    test

--- db/migrate/20130814042133_add_week_to_games.rb
+++ db/migrate/20130814042133_add_week_to_games.rb
@@ -1,5 +1,5 @@
 class AddWeekToGames < ActiveRecord::Migration
   def change
-    add_column :games, :week, :integer unless Game.attr_accessible(:week)
+    add_column :games, :week, :integer
   end
 end
