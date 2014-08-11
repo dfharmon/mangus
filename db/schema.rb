e76713fdff589181d4905dcce022eae74b8f68ef
dharmon <dharmon@avail-tvn.com>
2014-08-11 06:23:45 -0600

    2014 prep

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126234224) do
+ActiveRecord::Schema.define(:version => 20140811122121) do
 
   create_table "bets", :force => true do |t|
     t.integer  "game_id"
