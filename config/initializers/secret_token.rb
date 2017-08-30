1b46145cce96a0c201fc1efae7fd13888bb8a06b
dharmon <dusty@theharmons.com>
2017-08-30 12:43:04 -0600

    test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,3 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Mangus::Application.config.secret_token = '2d0ebba71560bff21e9ad131aef29d3c433390f5572c6ab40eade334421f435f1b53c1b938b00e2076ac566f5f38c8f91dda2cb45182c58554959af3a57def1f'
-config.secret_token = '2d0ebba71560bff21e9ad131aef29d3c433390f5572c6ab40eade334421f435f1b53c1b938b00e2076ac566f5f38c8f91dda2cb45182c58554959af3a57def1f'
